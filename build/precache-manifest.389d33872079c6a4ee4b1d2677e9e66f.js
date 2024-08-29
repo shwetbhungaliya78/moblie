8ef5a300e550da6a840c93958dc76e330964734d
shwetbhungaliya78 <shwetpatel928@gmail.com>
2024-08-29 15:48:00 +0530

    intial change

---
+++ build/precache-manifest.389d33872079c6a4ee4b1d2677e9e66f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6cbe433d2882664c3d05b7c2babd1d3",
+    "url": "/moblie/index.html"
+  },
+  {
+    "revision": "9eac25f28a2ee0110808",
+    "url": "/moblie/static/css/main.3274b984.chunk.css"
+  },
+  {
+    "revision": "395be05d07ec675b6b61",
+    "url": "/moblie/static/js/2.afeb26e6.chunk.js"
+  },
+  {
+    "revision": "9eac25f28a2ee0110808",
+    "url": "/moblie/static/js/main.823221c3.chunk.js"
+  },
+  {
+    "revision": "ca0fbad15d1fb7924675",
+    "url": "/moblie/static/js/runtime~main.8ab6477c.js"
+  }
+]);
